db6f303efaaf6d4c941ade22c2291d87cd8abeb8
dhidas-bnl-gov <dhidas@bnl.gov>
2015-12-14 10:35:04 -0500

    test for BField1DZRegularized

---
+++ exe/TestTBField1DZRegularized.cc
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////
+//
+// Dean Andrew Hidas <dhidas@bnl.gov>
+//
+// Created on: Fri Dec 11 16:53:32 EST 2015
+//
+////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+
+#include "TBField1DZRegularized.h"
+
+int TestTBField1DZRegularized (std::string const InFileName)
+{
+  TBField1DZRegularized TBF;
+  TBF.ReadFile(InFileName);
+  TBF.SaveAs("Saved2.dat");
+
+  return 0;
+}
+
+
+int main (int argc, char* argv[])
+{
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " [InFileName" << std::endl;
+    return 1;
+  }
+
+  TestTBField1DZRegularized(argv[1]);
+
+  return 0;
+}
