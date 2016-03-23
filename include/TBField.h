bd9f334ea8cca2a6618b0e60543ce7fa2e8eabd7
dhidas-bnl-gov <dhidas@bnl.gov>
2016-03-23 19:43:01 -0400

    rk4 updates for 3D

---
+++ include/TBField.h
@@ -0,0 +1,41 @@
+#ifndef GUARD_TBField_h
+#define GUARD_TBField_h
+////////////////////////////////////////////////////////////////////
+//
+// Dean Andrew Hidas <dhidas@bnl.gov>
+//
+// Created on: Fri Mar 18 17:13:44 EDT 2016
+//
+////////////////////////////////////////////////////////////////////
+
+
+class TBField
+{
+  // This class is designed to be a base class for a magnetic field object.
+
+  public:
+    virtual double GetBx (double const, double const, double const) = 0;
+    virtual double GetBy (double const, double const, double const) = 0;
+    virtual double GetBz (double const, double const, double const) = 0;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
