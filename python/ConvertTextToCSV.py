85aac6f15687b0a7d849ca113ce9624aa25ac6e8
dhidas-bnl-gov <dhidas@bnl.gov>
2016-07-01 13:58:59 -0400

    convert standard out to csv

---
+++ python/ConvertTextToCSV.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+
+
+def ConvertTextToCSV (fileName) :
+  f = open(fileName, 'r')
+
+  x = []
+  y = []
+  z = []
+
+
+  with open(fileName, 'r') as f:
+    for line in f:
+      v = line.split()
+      if (len(v) > 0 and v[0].startswith('#')):
+        continue
+  
+      if (len(v) < 3) :
+        continue
+  
+      x.append(float(v[0]))
+      y.append(float(v[1]))
+      z.append(float(v[2]))
+
+
+  x2 = list(set(x))
+  y2 = list(set(y))
+
+  nx = len(x2)
+  ny = len(y2)
+
+  x2.sort()
+  y2.sort()
+
+
+  print ' ,', ', '.join(map(str, x2))
+  for i in xrange(ny):
+    print str(y2[i]) + ',',
+    for j in xrange(nx):
+      if j is not nx - 1:
+        print str(z[i + j * nx]) + ',',
+      else:
+        print str(z[i + j * nx])
+
+
+
+
+
+if __name__ == "__main__":
+  if len(sys.argv) is not 2:
+    print 'Usage: ', sys.argv[1], ' [InFile]'
+    exit(0)
+
+  ConvertTextToCSV(sys.argv[1])
